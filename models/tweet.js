7a18fd64b53eb1c635818187f0b026b0730da1fc
giapandkien <ngan.bui@ryerson.ca>
2021-03-26 19:33:14 -0400

    tweet model & controller

--- models/tweet.js
+++ models/tweet.js
@@ -1,13 +1,18 @@
 var mongoose = require("mongoose");
-
+var validate = require("mongoose-validator");
+var contentValidate=[
+    validate({
+    arguments:[0,399],
+    message:'Tweet should be no more than 399 characters'
+})];
 const tweetSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    type: String,
+    content: String,
     required: true,
     created: Date,
     updated: Date,
     postedBy: {type: Schema.ObjectId, ref:'UserSchema'},
-    index: true
+    // validate: contentValidate
 })
 
 module.exports = mongoose.model("tweets",tweetSchema) ;
--- router.js
+++ router.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-let userController = require('./api-controllers/user-controllers');
-
+const userController = require('./api-controllers/user-controllers');
+const tweetController = require('./api-controllers/tweet-controllers');
+//USER
 router.post('/user/register', userController.registerUser);
 router.post('/user/login',userController.loginUser);
+
+//TWEET
+router.post('/tweet/create',tweetController.createTweet);
+router.get('/tweet/readAll/:id',tweetController.readAllTweet);
+router.get('/tweet/read/:id',tweetController.readTweet);
+router.put('/tweet/update/:id',tweetController.updateTweet);
+router.delete('/tweet/delete/:id',tweetController.deleteTweet);
 
 module.exports = router;
