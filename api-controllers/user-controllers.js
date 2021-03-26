22d4a09556d00caedb0dfb02d9e6091594785ee1
giapandkien <ngan.bui@ryerson.ca>
2021-03-26 11:22:23 -0400

    task 1 -section 1 completed

--- api-controllers/user-controllers.js
+++ api-controllers/user-controllers.js
@@ -3,81 +3,95 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 // let path = require("path");
 const User = require('../models/user');
-// let config = require('./../configs/config');
+let config = require('./../config');
 
 module.exports = {
     loginUser,
     registerUser
 };
 
+// HTTP STATUS CODE
+const STATUS_CODE = {
+    BAD_REQUEST: { status: 400, message: 'Bad request!' },
+    UNAUTHORIZED: { status: 401, message: 'Unauthorized!' },
+    NOT_FOUND: { status: 404, message: 'Not found!' },
+    FAILED_ACTION: { status: 424, message: 'Failed Dependency!' },
+    USERNAME_ALREADY_TAKEN: { status: 424, message: 'Username is already taken!' },
+    INTERNAL_ERROR: { status: 500, message: 'Internal Server Error!' },
+
+    SUCCESS_REGISTER: { status: 201, message: "Register successful!" },
+    FAILED_REGISTER: { status: 424, message: "Register failed!" },
+
+    SUCCESS_LOGIN: { status: 200, message: "Successfully logged in!" },
+}
+
 //CONTROLLERS
 
 function registerUser(req, res) {
-    let newUser = {
+    const { username, password } = req.body
+    const newUser = {
         _id: new mongoose.Types.ObjectId(),
-        username: req.body.username,
-        password: req.body.password
+        username: username,
+        password: password
     }
     register(newUser)
         .then(data => res.send(data))
-        .catch(err => res.send({ status: 0, message: err }))
+        .catch(err => handleException(err, res))
 }
 
 function loginUser(req, res) {
-
-    let username = req.body.username,
-        password = req.body.password;
+    const { username, password } = req.body
+    if (!username || !password) res.send({ status: 400, message: 'Bad request!' });
     checkUser({ username, password })
-        .then(data => {
-            return res.json(data);
-        })
-        .catch(err => res.send({ status: 0, message: err }))
+        .then(data => res.json(data))
+        .catch(err => handleException(err, res))
 }
 
 //HELPER FUNCTIONS
 
 async function checkUser({ username, password }) {
+    const user = await User.findOne({ username });
+    if (!user) return STATUS_CODE.NOT_FOUND;
 
-    const user = await User.findOne({ username })
-    if (!user)
-        return ({ status: 0, message: 'Username is not correct!' })
+    const validPassword = bcrypt.compareSync(password, user.password);
+    if (!validPassword) return STATUS_CODE.UNAUTHORIZED;
+    //assign login token
+    const token = jwt.sign({ userID: user.id }, config.secretString, { expiresIn: '1d' });
+    const dataUser = {
+        user: {
+            id: user.id,
+            username: user.username
+        }
+    };
+    const data = { dataUser, token }
+    return { ...STATUS_CODE.SUCCESS_LOGIN, data }
+}
 
-    if (user && bcrypt.compareSync(password, user.password)) {
-        //assign login token
-        const token = jwt.sign({ userID: user._id}, config.secretString, { expiresIn: '1d' });
-
-        return { status: 1, message: "Successfully logged in!", data: { user: user, token: token } }
-
-    }
-    else {
-        return { status: 0, message: "Password is not correct!", data: null };
-    }
+function handleException(error, res) {
+    console.log(error.message)
+    res.send(STATUS_CODE.INTERNAL_ERROR)
 }
 
 async function register(userParams) {
-
+    const { _id, username, password } = userParams;
     // check for unique username
-    const userValidate = await User.findOne({ username: userParams.username });
-
+    const userValidate = await User.findOne({ username });
     if (userValidate) {
-        return ({ status: 0, message: 'Username is already taken' })
+        // Failed Dependency
+        return STATUS_CODE.USERNAME_ALREADY_TAKEN
     }
-
-    const user = new User({
-        _id: userParams._id,
-        username: userParams.username,
-        password: userParams.password
-    });
-
-    const newuser = await user.save();
-
-
+    const registerUser = new User({ _id, username, password });
+    const newuser = await registerUser.save();
     if (newuser) {
-
-        return ({ status: 1, message: "Register successful !", data: { user: user }})
+        const data = {
+            user: {
+                id: newuser.id,
+                username: newuser.username
+            }
+        };
+        return { ...STATUS_CODE.SUCCESS_REGISTER, data };
     }
-    return ({ status: 0, message: "Register failed !" })
-
+    return STATUS_CODE.FAILED_REGISTER;
 }
 
 
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const server = require("http").Server(app);
 const mongoose = require('mongoose');
 const { MONGO_URI } = require('./config');
 
 const config = require("./config");
 const router = require("./router");
+
+// configure app to get data from a POST using bodyParser()
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 mongoose.set('useCreateIndex', true); //this to fix a deprecation
 
@@ -24,5 +29,3 @@ let PORT = config.PORT;
 server.listen(PORT, ()=>{
     console.log("server listen on port "+PORT+" with "+config.HOST);
 });
-
-
